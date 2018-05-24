6764dba341ef63ce036df720cb693b1cd310f422
mariuszpastuszka <mpastuszka@gmail.com>
2018-05-24 21:31:37 +0200

    [add] - adding LikedPost model class

---
+++ src/main/java/pl/mpas/microbloge_course/model/LikedPost.java
@@ -0,0 +1,64 @@
+package pl.mpas.microbloge_course.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class LikedPost {
+    User who;
+    UserPost likedPost;
+    Date date;
+
+    public LikedPost(User who, UserPost likedPost, Date date) {
+        this.who = who;
+        this.likedPost = likedPost;
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LikedPost likedPost1 = (LikedPost) o;
+        return Objects.equals(who, likedPost1.who) &&
+                Objects.equals(likedPost, likedPost1.likedPost) &&
+                Objects.equals(date, likedPost1.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(who, likedPost, date);
+    }
+
+    public User getWho() {
+        return who;
+    }
+
+    public void setWho(User who) {
+        this.who = who;
+    }
+
+    public UserPost getLikedPost() {
+        return likedPost;
+    }
+
+    public void setLikedPost(UserPost likedPost) {
+        this.likedPost = likedPost;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "LikedPost{" +
+                "who=" + who +
+                ", likedPost=" + likedPost +
+                ", date=" + date +
+                '}';
+    }
+}
