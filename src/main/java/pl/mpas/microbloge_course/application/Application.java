a760ec66eb637db4c6855469726ab245eb11385e
mariuszpastuszka <mpastuszka@gmail.com>
2018-05-28 19:38:15 +0200

    [add] - adding spring boot dependencies

---
+++ src/main/java/pl/mpas/microbloge_course/application/Application.java
@@ -0,0 +1,13 @@
+package pl.mpas.microbloge_course.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
