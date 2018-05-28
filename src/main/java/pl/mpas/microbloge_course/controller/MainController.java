fe36acae721d577ebb5014a90573b4dfc38d57e4
mariuszpastuszka <mpastuszka@gmail.com>
2018-05-28 21:01:39 +0200

    [add] - adding simple controller

--- src/main/java/pl/mpas/microbloge_course/controller/MainController.java
+++ src/main/java/pl/mpas/microbloge_course/controller/MainController.java
@@ -4,6 +4,8 @@ package pl.mpas.microbloge_course.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -13,6 +15,13 @@ public class MainController {
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "registration";
+    }
+
+    @RequestMapping(value = "/register", method = { RequestMethod.POST })
+    public String registerUser(@RequestParam(required = false) String login,
+                               @RequestParam(required = false) String password1,
+                               @@RequestParam(required = false) String password1) {
+        return "";
     }
 
 }
