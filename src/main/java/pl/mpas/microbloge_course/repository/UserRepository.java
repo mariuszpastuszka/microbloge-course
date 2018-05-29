a783459ed6547ba0df813d0469b872b96ea1c05f
mariuszpastuszka <mpastuszka@gmail.com>
2018-05-29 20:22:06 +0200

    [fix] - fixing problem with wrong location of properties file

--- src/main/java/pl/mpas/microbloge_course/repository/UserRepository.java
+++ src/main/java/pl/mpas/microbloge_course/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package pl.mpas.microbloge_course.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import pl.mpas.microbloge_course.model.User;
 
-public interface UserRepository implements  CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 }
