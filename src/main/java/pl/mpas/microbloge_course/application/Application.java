275505e497b0e13204be86871a8de8a72be5675b
mariuszpastuszka <mpastuszka@gmail.com>
2018-05-30 18:15:56 +0200

    [fix] - fixing package beans scanning

--- src/main/java/pl/mpas/microbloge_course/application/Application.java
+++ src/main/java/pl/mpas/microbloge_course/application/Application.java
@@ -3,7 +3,7 @@ package pl.mpas.microbloge_course.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "pl.mpas.microbloge_course")
 public class Application {
 
     public static void main(String[] args) {
