c6a74e66cc63d8b0f14e009c3eb389bd2d87e853
mariuszpastuszka <mpastuszka@gmail.com>
2018-06-06 21:05:10 +0200

    [add] - adding mail sender integration test

--- src/test/java/pl/mpas/microbloge_course/service/MailServiceTest.java
+++ src/test/java/pl/mpas/microbloge_course/service/MailServiceTest.java
@@ -2,19 +2,41 @@ package pl.mpas.microbloge_course.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import pl.mpas.microbloge_course.service.implementation.MailServiceImpl;
 
+import java.util.Properties;
+
 public class MailServiceTest {
 
-    MailService mailService = new MailServiceImpl(new JavaMailSenderImpl(), null);
+    MailService mailService = new MailServiceImpl(getJavaMailSender(), null);
+
     @Test
     public void sendMail() {
-        boolean result =  mailService.sendEmailToUser("john-doe@gmail.com",
+        boolean result =  mailService.sendEmailToUser("mpastuszka@gmail.com",
                 "Hello",
                 "Thanks for registration",
                 "microbloge-course");
 
         Assert.assertTrue(result);
     }
+
+    private JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setProtocol("smtp");
+
+        mailSender.setUsername("");
+        mailSender.setPassword("");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.quitwait", "false");
+
+        return mailSender;
+    }
+
 }
