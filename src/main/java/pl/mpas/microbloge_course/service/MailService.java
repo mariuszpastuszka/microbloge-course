eba7a0b75bddc77bb30e3efb782847e10f35f284
mariuszpastuszka <mpastuszka@gmail.com>
2018-06-07 18:46:07 +0200

    [add] - prototype of new method

--- src/main/java/pl/mpas/microbloge_course/service/MailService.java
+++ src/main/java/pl/mpas/microbloge_course/service/MailService.java
@@ -2,4 +2,5 @@ package pl.mpas.microbloge_course.service;
 
 public interface MailService {
     boolean sendEmailToUser(String address, String title, String message, String from);
+    boolean sendEmailToUserWithAttachment(String address, String title, String message, String from, String resourcePath);
 }
