aa74b672e3d159776a02f3d841523eeb40f9de2a
mariuszpastuszka <mpastuszka@gmail.com>
2018-06-07 20:28:43 +0200

    [add] - playing with threads cd..

--- src/test/java/pl/mpas/microbloge_course/service/ThreadExampleTest.java
+++ src/test/java/pl/mpas/microbloge_course/service/ThreadExampleTest.java
@@ -3,9 +3,30 @@ package pl.mpas.microbloge_course.service;
 import org.junit.Test;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class ThreadExampleTest {
     private Executor executor;
+
+    class Job implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    interface JobToDo {
+        boolean makeJob(String name);
+    }
+
+    Runnable job = new Runnable() {
+        @Override
+        public void run() {
+            System.out.println("Current thread name: " + Thread.currentThread().getName());
+            System.out.println("Mariusz");
+        }
+    };
 
     @Test
     public void sendJobToExecutor() {
@@ -17,5 +38,30 @@ public class ThreadExampleTest {
                 System.out.println("Mariusz");
             }
         }).start();
+    }
+
+    @Test
+    public void threadWithLambda() {
+        System.out.println("threadWithLambda() current thread name: " + Thread.currentThread().getName());
+
+        Runnable job = () -> {
+            System.out.println("Current thread name: " + Thread.currentThread().getName());
+            System.out.println("Mariusz");
+        };
+
+        Thread worker = new Thread(job);
+        worker.start();
+    }
+
+    @Test
+    public void executorThread() {
+;
+        Runnable job = () -> {
+            System.out.println("Sending emails from thread: " + Thread.currentThread().getName());
+        };
+
+        for (int i = 0; i < 100; i++) {
+            executor.execute(job);
+        }
     }
 }
