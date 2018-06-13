f6212dd5c87b8be7bc4a0448ecd7e32963f20343
mariuszpastuszka <mpastuszka@gmail.com>
2018-06-13 18:35:10 +0200

    [add] - example of bean configuration file

--- src/main/java/pl/mpas/microbloge_course/application/Configuration.java
+++ src/main/java/pl/mpas/microbloge_course/application/Configuration.java
@@ -2,8 +2,19 @@ package pl.mpas.microbloge_course.application;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.hibernate5.HibernateExceptionTranslator;
+import pl.mpas.microbloge_course.controller.MainController;
+import pl.mpas.microbloge_course.service.UserService;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
 
+   @Bean
+   public MainController mainController() {
+       return new MainController(userService());
+   }
+
+   @Bean
+    public UserService userService() {
+       return new UserService();
+   }
 }
